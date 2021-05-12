920e8533f7a7ec0d2ba10f77dcbd2d6a4aaa31c2
Addisalem Wordofa <addaber@gmail.com>
2021-05-12 11:44:08 -0400

    #2 - /math/sum completed

--- src/main/java/com/example/demo/RequestsResponsesDemo.java
+++ src/main/java/com/example/demo/RequestsResponsesDemo.java
@@ -1,8 +1,13 @@
 package com.example.demo;
 
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Map;
 
 @RestController
 public class RequestsResponsesDemo {
@@ -12,32 +17,42 @@ public class RequestsResponsesDemo {
     }
 
     @GetMapping("/math/pi")
-    public String mathPi(){
+    public String mathPi() {
         return "3.141592653589793";
 
     }
+
     @GetMapping("/math/calculate")
-    public String calculate(@RequestParam(defaultValue = "null") String operation,@RequestParam Integer x, @RequestParam Integer y){
-        if(operation.equals("add")){
+    public String calculate(@RequestParam(defaultValue = "null") String operation, @RequestParam Integer x, @RequestParam Integer y) {
+        if (operation.equals("add")) {
             //int result=Integer.parseInt(x)+Integer.parseInt(y);
-            Integer result=x+y;
-            return String.valueOf(x)+"+"+String.valueOf(y)+"="+String.valueOf(result);
-        }
-        else if(operation.equals("multiply")){
-            Integer result=x*y;
-            return  String.valueOf(x)+"*"+String.valueOf(y)+"="+String.valueOf(result);
-        }
-        else if(operation.equals("subtract")){
-            Integer result=x-y;
-            return  String.valueOf(x)+"-"+String.valueOf(y)+"="+String.valueOf(result);
-        }
-        else if(operation.equals("divide")){
-            Integer result=x/y;
-            return String.valueOf(x)+"/"+String.valueOf(y)+"="+String.valueOf(result);
-        }else {
+            Integer result = x + y;
+            return String.valueOf(x) + "+" + String.valueOf(y) + "=" + String.valueOf(result);
+        } else if (operation.equals("multiply")) {
+            Integer result = x * y;
+            return String.valueOf(x) + "*" + String.valueOf(y) + "=" + String.valueOf(result);
+        } else if (operation.equals("subtract")) {
+            Integer result = x - y;
+            return String.valueOf(x) + "-" + String.valueOf(y) + "=" + String.valueOf(result);
+        } else if (operation.equals("divide")) {
+            Integer result = x / y;
+            return String.valueOf(x) + "/" + String.valueOf(y) + "=" + String.valueOf(result);
+        } else {
             Integer result = x + y;
             return String.valueOf(x) + "+" + String.valueOf(y) + "=" + String.valueOf(result);
         }
     }
 
+    @PostMapping("/math/sum")
+    public String calculateSumOfNumbers(@RequestParam List<Integer> n) {
+        Integer sum = 0;
+        StringBuilder str=new StringBuilder();
+        //List values = null;
+        for(Integer num:n){
+            sum=sum+num;
+            str.append(num+"+");
+        }
+        str.deleteCharAt(str.length() - 1);
+        return str.toString()+ "=" + String.valueOf(sum);
+    }
 }
